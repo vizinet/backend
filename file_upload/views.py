7f74efb20fdc86b79c792899b99d1431acb84b41
theBrian <kungfunub@gmail.com>
2017-07-24 14:58:30 -0700

    Fixed comma error

--- file_upload/views.py
+++ file_upload/views.py
@@ -105,7 +105,7 @@ def create_algorithm_two_object_json(Picture, json):
 	try:
 		newAlg1 = AlgorithmTwo(
 			picture = Picture,
-			image2 = json['imageTwo']
+			image2 = json['imageTwo'],
 			nearX = json['nearTargetX'], 
 			nearY = json['nearTargetY'],				
 			farX = json['farTargetX'],
