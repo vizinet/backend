2e2245508a3352b72e77656db1388a96903569df
Luke Weber <lukedottec@gmail.com>
2017-09-11 17:04:42 -0700

    Bunch of small changes

--- file_upload/urls.py
+++ file_upload/urls.py
@@ -3,8 +3,8 @@
 Copyright © 2017,
 Laboratory for Atmospheric Research at Washington State University,
 All rights reserved.
-
 """
+
 from django.conf.urls import url
 
 from . import views
@@ -12,6 +12,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^upload$', views.upload, name='upload'),
-    #url(r'^test$',views.test, name='test'),
     url(r'^delete/(?P<id>\d+)/$', views.delete_picture, name="delete_picture")
 ]
