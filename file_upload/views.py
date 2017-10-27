2c467d0084a985405cf6093c7d0082831017b89e
Luke Weber <lukedottec@gmail.com>
2017-10-26 17:18:56 -0700

    H

--- file_upload/views.py
+++ file_upload/views.py
@@ -574,8 +574,6 @@ def view_picture(request, picId=-1, comment_num=1):
         if len(alg) > 0:
             computed_vr = alg[0].calculatedVisualRange
 
-        print "computed_vr: " + computed_vr
-
         this_comment_form = comment_form()
 
         # Setup range of image numbers for the
